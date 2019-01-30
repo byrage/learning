c6c49b5c13a652a2bc1ce3f1f539fbe41a7a1b98
byrage <byrage91@gmail.com>
2019-01-30 12:48:13 +0900

    Create GeometryTest

---
+++ java/src/test/java/com/tistory/bryage/GeometryTest.java
@@ -0,0 +1,45 @@
+package com.tistory.bryage;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.impl.CoordinateArraySequence;
+import org.locationtech.jts.geom.impl.CoordinateArraySequenceFactory;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class GeometryTest {
+
+    /*
+    @See http://esri.github.io/geometry-api-java/doc/Polygon.html
+    */
+    @Test
+    public void createPolygon() {
+
+        // given
+        List<Coordinate> coordinates = Lists.newArrayList(
+                new Coordinate(34.877737, 128.6917877),
+                new Coordinate(34.8721037, 128.6816597),
+                new Coordinate(34.8530887, 128.6845779),
+                new Coordinate(34.853793, 128.7118721),
+                new Coordinate(34.877737, 128.6917877));
+
+        CoordinateArraySequence coordinateArraySequence = new CoordinateArraySequence(coordinates.toArray(new Coordinate[] {}));
+
+        GeometryFactory geometryFactory = new GeometryFactory(CoordinateArraySequenceFactory.instance());
+        LinearRing outerRing = new LinearRing(coordinateArraySequence, geometryFactory);
+
+        // when
+        Polygon polygon = new Polygon(outerRing, null, geometryFactory);
+
+        // then
+        assertThat(polygon.getExteriorRing().toString(), is(equalTo("<LINEARRING (34.877737 128.6917877, 34.8721037 128.6816597, 34.8530887 128.6845779, 34.853793 128.7118721, 34.877737 128.6917877)>")));
+        assertThat(polygon.getLength(), is(equalTo(0.08938231853062086d)));
+        assertThat(polygon.getArea(), is(equalTo(4.383503918897806E-4d)));
+
+    }
+}
