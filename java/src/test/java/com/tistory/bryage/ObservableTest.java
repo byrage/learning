ea8501c56286b975b5dfcc886612ca7ecf848b65
byrage <byrage91@gmail.com>
2019-01-30 12:48:06 +0900

    Create ObservableTest

---
+++ java/src/test/java/com/tistory/bryage/ObservableTest.java
@@ -0,0 +1,62 @@
+package com.tistory.bryage;
+
+import org.junit.Test;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ObservableTest {
+
+    @Test
+    public void singleObserver() {
+
+        Observer o1 = (o, arg) -> System.out.println(
+                Thread.currentThread().getName() +
+                        " / observable=" + o.toString() +
+                        " / arg=" + arg);
+
+        IntObservable intObservable = new IntObservable();
+        intObservable.addObserver(o1);
+
+        ExecutorService executors = Executors.newSingleThreadExecutor();
+        executors.submit(intObservable);
+
+        System.out.println(Thread.currentThread().getName() + " / EXIT");
+    }
+
+    @Test
+    public void multipleObserver() {
+
+        Observer o1 = (o, arg) -> System.out.println(
+                Thread.currentThread().getName() +
+                        " / observable=" + o.toString() +
+                        " / arg=" + arg);
+
+        Observer o2 = (o, arg) -> System.out.println(
+                Thread.currentThread().getName() +
+                        " just hello");
+
+        IntObservable intObservable = new IntObservable();
+        intObservable.addObserver(o1);
+        intObservable.addObserver(o2);
+
+        ExecutorService executors = Executors.newSingleThreadExecutor();
+        executors.submit(intObservable);
+
+        System.out.println(Thread.currentThread().getName() + " / EXIT");
+    }
+
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 10; i++) {
+                setChanged();
+                notifyObservers(i);
+            }
+        }
+    }
+}
