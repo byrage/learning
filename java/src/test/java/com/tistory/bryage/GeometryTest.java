4f864b73c528f525dedbb3ecdd8013e6888db796
byrage <byrage91@gmail.com>
2019-02-20 23:03:19 +0900

    Modify GeometryTest

--- java/src/test/java/com/tistory/bryage/GeometryTest.java
+++ java/src/test/java/com/tistory/bryage/GeometryTest.java
@@ -21,6 +21,7 @@ public class GeometryTest {
     public void createPolygon() {
 
         // given
+        GeometryFactory geometryFactory = new GeometryFactory(CoordinateArraySequenceFactory.instance());
         List<Coordinate> coordinates = Lists.newArrayList(
                 new Coordinate(34.877737, 128.6917877),
                 new Coordinate(34.8721037, 128.6816597),
@@ -29,17 +30,16 @@ public class GeometryTest {
                 new Coordinate(34.877737, 128.6917877));
 
         CoordinateArraySequence coordinateArraySequence = new CoordinateArraySequence(coordinates.toArray(new Coordinate[] {}));
-
-        GeometryFactory geometryFactory = new GeometryFactory(CoordinateArraySequenceFactory.instance());
         LinearRing outerRing = new LinearRing(coordinateArraySequence, geometryFactory);
 
         // when
-        Polygon polygon = new Polygon(outerRing, null, geometryFactory);
+        Polygon polygon = new Polygon(outerRing, null, geometryFactory); // shell : outer boundary, holes : inner boundary
+//        Polygon polygon = geometryFactory.createPolygon(coordinateArraySequence);
 
         // then
-        assertThat(polygon.getExteriorRing().toString(), is(equalTo("<LINEARRING (34.877737 128.6917877, 34.8721037 128.6816597, 34.8530887 128.6845779, 34.853793 128.7118721, 34.877737 128.6917877)>")));
+        assertThat(polygon.getExteriorRing().toString(), is(equalTo("LINEARRING (34.877737 128.6917877, 34.8721037 128.6816597, 34.8530887 128.6845779, 34.853793 128.7118721, 34.877737 128.6917877)")));
         assertThat(polygon.getLength(), is(equalTo(0.08938231853062086d)));
         assertThat(polygon.getArea(), is(equalTo(4.383503918897806E-4d)));
-
+        assertThat(polygon.contains(geometryFactory.createPoint(new Coordinate(34.877737, 128.6917877))), is(true));
     }
 }
